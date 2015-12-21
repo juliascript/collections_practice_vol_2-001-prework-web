182bc84294caadb271c3087d28db4b6d4d530745
juliascript <juliageist23@gmail.com>
2015-12-21 13:45:17 +0000

    nitrous-backup at Mon Dec 21 13:45:17 UTC 2015

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,19 @@
 # your code goes here
+def begins_with_r(array)
+  answer = array.select { |each| each.start_with?("r")}
+  if answer == array
+    true
+  else
+    false
+  end
+end
+
+def contain_a(array)
+  contains_a = array.select { |each| each.include?("a")}
+end
+
+def first_wa(array)
+  array.each do |each|
+    bool = each.start_with?("wa")
+  end
+end
