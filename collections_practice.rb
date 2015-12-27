4cf855d6b81e1ba90f13cb1c456d4bbe5c863c94
juliascript <juliageist23@gmail.com>
2015-12-27 13:27:09 +0000

    nitrous-backup at Sun Dec 27 13:27:09 UTC 2015

--- collections_practice.rb
+++ collections_practice.rb
@@ -13,7 +13,26 @@ def contain_a(array)
 end
 
 def first_wa(array)
-  array.each do |each|
-    bool = each.start_with?("wa")
+  array.find do |each|
+    if (each.is_a?(Symbol))
+      each = each.to_s
+    end
+    each.start_with?("wa")
+  end
+end
+
+def remove_non_strings(array)
+  array.select do |each|
+    each.is_a?(String)
+  end
+end
+
+def count_elements(array)
+  count = Hash.new 0
+  array.each {|each| count[each] += 1}
+  resulting_array = []
+  count.keys.each do |result|
+    result[:count] = count[result]
+    resulting_array << result
   end
 end
